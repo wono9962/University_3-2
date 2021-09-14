2e85534e3eebb7264eab90044eab43025ae0496b
wono9962 <wono9962@gmail.com>
2021-09-14 23:44:32 +0900

    2week file commit

---
+++ Bigdata_Practice/2week/2.py
@@ -0,0 +1,42 @@
+# [기본자료형]
+a = 3
+b = 1.2
+print(a)
+print(b)
+print(a+b)
+a = 0x12A
+print(a)
+print("===================================")
+
+# [논리형]
+b = True
+print(b)
+b = False
+print(b)
+print("===================================")
+
+# [사칙연산]
+a = 3
+b = 4
+print(a+b)
+print(a-b)
+print(a*b)
+print(a/b)
+print(a**b)
+print(a%b)
+print(7%3)
+print(a//b)
+print("===================================")
+
+# [문자형]
+a = 'abc'
+print(a)
+a = "abc"
+print(a)
+a = '''abc'''
+print(a)
+a = """abc"""
+print(a)
+a = """abc
+abc"""
+print(a)
