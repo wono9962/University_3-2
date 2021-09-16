c98cb9978bb28a69042665af2fbd177b65de911c
wono9962 <wono9962@gmail.com>
2021-09-16 22:33:56 +0900

    seconde file commit

--- Bigdata_Practice/2week/2.py
+++ Bigdata_Practice/2week/2.py
@@ -40,3 +40,68 @@ print(a)
 a = """abc
 abc"""
 print(a)
+
+# [문자열]
+s1 = 'Hello Python'
+print(s1)
+
+head = "Python"
+tail = " is fun"
+print(head)
+print(tail)
+print(head+tail)
+print(head*2)
+print("="*5)
+a = "Now is better than never"
+print(a[0])
+print(a[4])
+print(a[-1])
+print(a[-2])
+b = a[0] + a[1] + a[2]
+print(b)
+print(a[0:3])
+print(a[4:6])
+print(a[19:])
+print(a[:3])
+print(a[:])
+print(a[7:-11])
+print("===================================")
+
+a = "Python"
+print(a)
+print(a.count('p'))
+print(a.count('P'))
+print(a.find('y'))
+print(a.find('p'))
+print(a.index('y'))
+print(a.index('p'))
+print("===================================")
+b = ","
+c = b.join('Abcd')
+print(c)  # 'A, b, c, d'
+print(a.upper())  # 'PYTHON'
+print(a.lower())  # 'python'
+d = "      py       "
+print(d)
+print(d.lstrip())
+print(d.rstrip())
+print(d.strip())
+a = "Pithon"
+print(a)
+a = "Python is difficult."
+print(a.replace("difficult", "funny"))
+print(a.split())
+b = "a, b, c, d"
+print(b)
+print(b.split(","))
+print("===================================")
+
+# [리스트 자료형]
+a = [1, 2, 3]
+print(a)
+b = ['Life', 'is', 'too', 'short']
+print(b)
+c = [1, 2, 'Life', "is"]
+print(c)
+d = [1, 2, [3, 4], ['Life', 'is']]
+print(d)
